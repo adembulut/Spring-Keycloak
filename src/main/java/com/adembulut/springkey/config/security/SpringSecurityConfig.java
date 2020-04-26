21c3cda610ceee83efa18534544a83255c9d1ef4
Adem Bulut <adem.bulut@etstur.com>
2020-04-26 15:57:06 +0300

    keycloak test
    Co-Authored-By: Adem Bulut <adembulutapp@gmail.com>

--- src/main/java/com/adembulut/springkey/config/security/SpringSecurityConfig.java
+++ src/main/java/com/adembulut/springkey/config/security/SpringSecurityConfig.java
@@ -3,56 +3,110 @@ package com.adembulut.springkey.config.security;
 import com.adembulut.springkey.config.security.handler.CustomAccessDeniedHandler;
 import com.adembulut.springkey.config.security.handler.CustomAuthenticationFailureHandler;
 import com.adembulut.springkey.config.security.handler.CustomLogoutSuccessHandler;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 /**
  * @author adem.bulut  on  26.04.2020
  * @project springkey
  */
 @Configuration
-@EnableWebSecurity(debug = true)
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity(debug = false)
+@KeycloakConfiguration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    @Autowired
+    public KeycloakClientRequestFactory keycloakClientRequestFactory;
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password(passwordEncoder().encode("user1")).roles("USER")
-                .and()
-                .withUser("user2").password(passwordEncoder().encode("user2")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+//
+//    @Override
+//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user1").password(passwordEncoder().encode("user1")).roles("USER")
+//                .and()
+//                .withUser("user2").password(passwordEncoder().encode("user2")).roles("USER")
+//                .and()
+//                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+//    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception
+//    {
+//        super.configure(http);
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/user*").hasAuthority("USER")
+//                .antMatchers("/admin*").hasAuthority("ADMIN")
+//                .anyRequest().permitAll()
+//                .anyRequest().authenticated();
+//    }
+
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http
+        super.configure(http);//test
+        http.headers().httpStrictTransportSecurity().disable()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/user/**").hasAuthority("USER")
                 .antMatchers("/anonymous*").permitAll()
                 .antMatchers("/**").permitAll()
                 .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/index", true)
-                //.failureUrl("/login.html?error=true")
-                .failureHandler(customAuthenticationFailureHandler())
                 .and()
                 .logout()
-                .logoutUrl("/perform_logout")
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(customLogoutSuccessHandler());
+                .logoutUrl("/logout");
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/perform_login")
+//                .defaultSuccessUrl("/index", true)
+//                //.failureUrl("/login.html?error=true")
+//                .failureHandler(customAuthenticationFailureHandler())
+//                .and()
+//                .logout()
+//                .logoutUrl("/perform_logout")
+//                .deleteCookies("JSESSIONID")
+//                .logoutSuccessHandler(customLogoutSuccessHandler());
     }
 
     @Bean
@@ -61,17 +115,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CustomAccessDeniedHandler customAccessDeniedHandler(){
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
         return new CustomAccessDeniedHandler();
     }
 
     @Bean
-    public CustomAuthenticationFailureHandler customAuthenticationFailureHandler(){
+    public CustomAuthenticationFailureHandler customAuthenticationFailureHandler() {
         return new CustomAuthenticationFailureHandler();
     }
 
     @Bean
-    public CustomLogoutSuccessHandler customLogoutSuccessHandler(){
+    public CustomLogoutSuccessHandler customLogoutSuccessHandler() {
         return new CustomLogoutSuccessHandler();
     }
+
 }
